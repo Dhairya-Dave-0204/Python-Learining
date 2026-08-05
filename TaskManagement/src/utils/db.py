15ac646f8d7713a828370b5ec5a3958e70b59188
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-05 10:33:24 +0530

    Code the db file to connect with DB using sql alchemy

---
+++ TaskManagement/src/utils/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from src.utils.settings import settings
+
+Base = declarative_base()
+
+engine = create_engine(url= settings.DB_CONNECTION)
+
+Local_Session = sessionmaker(bind = engine)
+
+def get_db():
+    session = Local_Session()
+    try:
+        yield session
+    except:
+        return { "status": False, "message": "Error in getting the DB session" }
+    finally:
+        session.close()
