ee56e14edb75d18154d5287623b42522703cf874
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-05 11:11:47 +0530

    Optimize the db pool and the hardcode of connection url

--- TaskManagement/src/utils/db.py
+++ TaskManagement/src/utils/db.py
@@ -1,3 +1,8 @@
+# By default the sql alchemy creates a pool of 5 connections when the create engine function is called
+# one can explicitly define the pool attributes like follows for more personalizations
+
+# Sqlalchemy is lazy so it only processes the connection when a query (read / write) is to be executed
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -5,7 +10,13 @@ from src.utils.settings import settings
 
 Base = declarative_base()
 
-engine = create_engine(url= settings.DB_CONNECTION)
+engine = create_engine(
+    url=settings.DB_CONNECTION,
+    pool_size=10,         # Keep up to 10 connections open and ready
+    max_overflow=20,      # If the pool is full, allow up to 20 extra temporary connections
+    pool_timeout=30,      # If no connections are available, wait 30 seconds before throwing an error
+    pool_recycle=1800     # Reconnect connections older than 30 minutes to prevent database timeouts
+)
 
 Local_Session = sessionmaker(bind = engine)
 
--- TaskManagement/src/utils/settings.py
+++ TaskManagement/src/utils/settings.py
@@ -3,6 +3,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Setting(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    DB_CONNECTION: str
+    POSTGRE_USER: str
+    POSTGRE_PASSWORD: str
+    POSTGRE_HOST: str
+    POSTGRE_PORT: str = "5432" # You can provide a default value if you want
+    POSTGRE_DB: str
 
-settings = Setting()
+@property
+def DB_CONNECTION(self) -> str:
+    """
+        Dynamically builds the database URL.
+        Format: postgresql://user:password@host:port/database
+    """
+
+    return f"postgresql://{self.POSTGRE_USER}:{self.POSTGRE_PASSWORD}@{self.POSTGRE_HOST}:{self.POSTGRE_PORT}/{self.POSTGRE_DB}"
