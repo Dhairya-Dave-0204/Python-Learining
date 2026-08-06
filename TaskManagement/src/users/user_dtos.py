c91be4e1fcf3169ced9d8b7e90a26c1b2ce0ea17
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-06 13:43:23 +0530

    Solve the error in user schema in dtos file

--- TaskManagement/src/users/user_dtos.py
+++ TaskManagement/src/users/user_dtos.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 class UserSchema(BaseModel):
-    name = str
-    username = str
-    password = str
-    email = str
+    name: str
+    username: str
+    password: str
+    email: str
