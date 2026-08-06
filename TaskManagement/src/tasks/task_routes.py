9a2e110b046b97d53282de5856306427d9e2fe0e
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-06 11:37:39 +0530

    Define the status code for each route

--- TaskManagement/src/tasks/task_routes.py
+++ TaskManagement/src/tasks/task_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from src.tasks import task_controller
 from src.tasks.task_dtos import TaskSchema
@@ -6,22 +6,22 @@ from src.utils.db import get_db
 
 task_routes = APIRouter(prefix= "/tasks")
     
-@task_routes.post("/create")
+@task_routes.post("/create", status_code=status.HTTP_201_CREATED)
 def create_task(body: TaskSchema, db = Depends(get_db)):
     return task_controller.create_task(body, db)
 
-@task_routes.get("/all-tasks")
+@task_routes.get("/all-tasks", status_code=status.HTTP_200_OK)
 def get_tasks(db = Depends(get_db)):
     return task_controller.get_tasks(db)
 
-@task_routes.get("/get/{task_id}")
+@task_routes.get("/get/{task_id}", status_code=status.HTTP_200_OK)
 def get_task_by_id(task_id: int, db = Depends(get_db)):
     return task_controller.get_task_by_id(task_id, db)
 
-@task_routes.put("/update/{task_id}")
+@task_routes.put("/update/{task_id}", status_code=status.HTTP_201_CREATED)
 def update_task_by_id(body: TaskSchema, task_id: int,  db = Depends(get_db)):
     return task_controller.update_task_by_id(body, task_id, db)
 
-@task_routes.delete("/delete/{task_id}")
+@task_routes.delete("/delete/{task_id}", status_code=status.HTTP_200_OK)
 def delete_task_by_id(task_id: int,  db = Depends(get_db)):
     return task_controller.delete_task_by_id(task_id, db)
