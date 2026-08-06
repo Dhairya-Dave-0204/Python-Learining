eca1d8f7833b472a42f49f39076e647a95720635
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-06 12:18:56 +0530

    Connect the test route to the main file

--- TaskManagement/main.py
+++ TaskManagement/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.utils.db import Base, engine
 from src.tasks.task_routes import task_routes
+from src.users.user_routes import user_router
 
 Base.metadata.create_all(engine)
 
@@ -10,3 +11,4 @@ app = FastAPI(
 )
 
 app.include_router(task_routes)
+app.include_router(user_router)
