b440ae8d6d5546dd8d356d98d24018feb45b79bb
Dhairya Dave <dhairyadave962@gmail.com>
2026-08-06 13:56:48 +0530

    Create the proper controller to register user

--- TaskManagement/src/users/user_controller.py
+++ TaskManagement/src/users/user_controller.py
@@ -1,8 +1,35 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from pwdlib import PasswordHash
 
 from src.users.user_dtos import UserSchema
+from src.users.user_model import UserModel
 
+password_hash = PasswordHash.recommended()
+
+def get_password_hash(password):
+    return password_hash.hash(password)
 
 def register_user(body: UserSchema, db: Session):
-    print(body)
-    return { "status": True, "message": "User registration successful!" }
+    is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
+    if is_user:
+        raise HTTPException(400, "User already exists with this username")
+
+    is_user = db.query(UserModel).filter(UserModel.email == body.email).first()
+    if is_user:
+        raise HTTPException(400, "User already exists with this email")
+
+    hash_pass = get_password_hash(body.password)
+
+    new_user = UserModel(
+        name = body.name,
+        username = body.username,
+        password = hash_pass,
+        email = body.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return { "status": True, "message": "User registration successful!", "data": new_user }
